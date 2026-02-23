b55dd12a4245ea1560dee53bec740fb7e2f96cf4
wanshushan <3445497806@qq.com>
2026-02-23 10:57:08 +0800

    自定义域名

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://wanshushan.github.io",
-	base: "/blog",
+	site: "https://blog.wanshushan.top",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
